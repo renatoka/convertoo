106c0a178aca9be7ef823fb91157ad9371ff299a
Renato <rkauric@vvg.hr>
2022-11-15 16:30:37 +0100

    Flask server that runs everything

--- app.py
+++ app.py
@@ -1,40 +1,84 @@
-from flask import Flask, send_from_directory, Response
 import os
-from datetime import datetime
-import time
+import secrets
+import string
+import pythoncom
+import firebase_admin
+from docx2pdf import convert
+from firebase_admin import credentials, storage
+from flask import Flask, Response, send_from_directory, redirect, request
+from uuid import uuid4
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+from dotenv import load_dotenv
+
+load_dotenv()
+
+alphabet = string.ascii_letters + string.digits
 
 app = Flask(__name__)
+app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10MB
+cred = credentials.Certificate("secret.json")
+firebase_admin.initialize_app(cred, {"storageBucket": os.getenv("STORAGE_BUCKET")})
+bucket = storage.bucket(os.getenv("STORAGE_BUCKET"))
 
-react_folder = 'streaming-tutorial'
-directory= os.getcwd()+ f'/{react_folder}/build/static'
+react_folder = "frontend"
+directory = os.getcwd() + f"/{react_folder}/build/static"
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    ''' User will call with with thier id to store the symbol as registered'''
-    path= os.getcwd()+ f'/{react_folder}/build'
+    """User will call with with thier id to store the symbol as registered"""
+    path = os.getcwd() + f"/{react_folder}/build"
     print(path)
-    return send_from_directory(directory=path,path='index.html')
+    return send_from_directory(directory=path, path="index.html")
+
 
 #
-@app.route('/static/<folder>/<file>')
-def css(folder,file):
-    ''' User will call with with thier id to store the symbol as registered'''
-    
-    path = folder+'/'+file
-    return send_from_directory(directory=directory,path=path)
+@app.route("/static/<folder>/<file>")
+def css(folder, file):
+    """User will call with with thier id to store the symbol as registered"""
 
-@app.route('/stream')
-def stream():
+    path = folder + "/" + file
+    return send_from_directory(directory=directory, path=path)
 
-    def get_data():
 
-        while True:
-            #gotcha
-            time.sleep(1)
-            yield f'data: {datetime.now().second} \n\n'
+@app.route("/", methods=["POST"])
+def upload():
+    pythoncom.CoInitialize()
+    file = request.files["file"]
+    try:
+        if file.filename.endswith(".docx"):
+            random_string = "".join(secrets.choice(alphabet) for i in range(10))
 
-    return Response(get_data(), mimetype='text/event-stream')
+            file.save(f"./uploads/{secure_filename(file.filename)}")
+            convert(
+                f"./uploads/{secure_filename(file.filename)}",
+                f"./uploads/{secure_filename(file.filename)}.pdf",
+            )
 
-if __name__ == '__main__':
+            blob = bucket.blob(
+                f"{secure_filename(random_string)}/{secure_filename(file.filename)}.pdf"
+            )
+
+            token = uuid4()
+            metadata = {"firebaseStorageDownloadTokens": token}
+            blob.metadata = metadata
+
+            blob.upload_from_filename(f"./uploads/{secure_filename(file.filename)}.pdf")
+            blob.make_public()
+
+            os.remove(f"./uploads/{secure_filename(file.filename)}")
+            os.remove(f"./uploads/{secure_filename(file.filename)}.pdf")
+
+            return redirect(blob.public_url)
+        else:
+            return redirect("Not a docx file")
+
+    except RequestEntityTooLarge as e:
+        return "File too large. Max file size is 10MB"
+
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
     app.run(debug=True)
