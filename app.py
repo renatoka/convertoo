5e4cc1f6c8db03da56e1a5c671eae84c73d51c8d
Renato <rkauric@vvg.hr>
2022-11-16 21:20:04 +0100

    I think I fixed rendering

--- app.py
+++ app.py
@@ -4,25 +4,36 @@ import string
 import firebase_admin
 import convertapi
 from firebase_admin import credentials, storage
-from flask import Flask, Response, render_template, redirect, request, send_from_directory
+from flask import (
+    Flask,
+    Response,
+    render_template,
+    redirect,
+    request,
+    send_from_directory,
+)
 from uuid import uuid4
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 from dotenv import load_dotenv
+
 load_dotenv()
 
 alphabet = string.ascii_letters + string.digits
-convertapi.api_secret = 'H2LgxucHyJVVY2DG'
+convertapi.api_secret = "H2LgxucHyJVVY2DG"
 
-react_folder = 'frontend'
-directory= os.getcwd()+ f'/{react_folder}/build/static'
+react_folder = "frontend"
+directory = os.getcwd() + f"/{react_folder}/build/static"
 
-app = Flask(__name__)
+app = Flask(
+    __name__, template_folder="template", static_folder=directory, static_url_path=""
+)
 
 app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10MB
 cred = credentials.Certificate("secret.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "convertme-a0b9f.appspot.com"})
 bucket = storage.bucket("convertme-a0b9f.appspot.com")
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -40,7 +51,6 @@ def upload():
                 },
                 from_format="docx",
             ).save_files(f"./uploads/{random_string}.pdf")
-            
 
             blob = bucket.blob(f"{random_string}/{random_string}.pdf")
 
@@ -64,21 +74,21 @@ def upload():
     except Exception as e:
         return str(e)
 
+
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    path= os.getcwd()+ f'/{react_folder}/build'
-    print(path)
-    return send_from_directory(directory=path,path='index.html')
+    return render_template("index.html")
 
 
-@app.route('/static/<folder>/<file>')
-def css(folder,file):
-    path = folder+'/'+file
-    return send_from_directory(directory=directory,path=path)
+@app.route("/static/<folder>/<file>")
+def css(folder, file):
+    path = folder + "/" + file
+    return send_from_directory(directory=directory, path=path)
 
 
 if __name__ == "__main__":
