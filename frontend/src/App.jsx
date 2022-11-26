12839f5db1ce015d648a9f883091944e3c8fd46f
Renato <rkauric@vvg.hr>
2022-11-26 19:59:17 +0100

    Removed unused import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Bottom from "./components/Bottom";
-import { useTranslation } from "react-i18next";
 
 export default function App() {
   return (
