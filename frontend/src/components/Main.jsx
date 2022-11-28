2482b575d524bc66ad472f58aa1cd89bb4ced7fe
Renato <rkauric@vvg.hr>
2022-11-28 16:31:14 +0100

    Moved error and upload notifications to new location on main page

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,12 +2,14 @@ import { useTranslation } from "react-i18next"
 import { useState } from "react";
 import FormatError from "./chakra/FormatError"
 import SizeError from "./chakra/SizeError"
+import SuccessUpload from "./chakra/SuccessUpload"
 
 const Main = () => {
 
   const { t } = useTranslation()
   const [formatError, setFormatError] = useState(false)
   const [sizeError, setSizeError] = useState(false)
+  const [success, setSuccess] = useState(false)
 
   const handlePostMethod = (event) => {
     event.preventDefault()
@@ -15,9 +17,12 @@ const Main = () => {
     const formData = new FormData()
     formData.append("file", files[0])
 
-    if (files[0].type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
-      if (files[0].size <= 10000000) {
-        document.getElementById('forma').submit()
+    if (files[0].type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") { // .docx
+      if (files[0].size <= 10000000) { // 10MB
+        setSuccess(true)
+        setInterval(() => {
+          document.getElementById('forma').submit();
+        }, 2000)
       } else {
         setSizeError(true)
         setTimeout(() => {
@@ -37,7 +42,7 @@ const Main = () => {
     <>
       <section className="relative overflow-hidden">
         {/* Illustration behind hero content */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-10" aria-hidden="true">
+        <div className="absolute left-1/2 transform -translate-x-1/2 pointer-events-none -z-10" aria-hidden="true">
           <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
@@ -47,8 +52,8 @@ const Main = () => {
               </linearGradient>
             </defs>
             <g fill="url(#illustration-01)" fillRule="evenodd">
-              <circle cx="1232" cy="128" r="128" />
-              <circle cx="155" cy="443" r="64" />
+              <circle cx="1232" cy="228" r="88" />
+              <circle cx="155" cy="443" r="124" />
             </g>
           </svg>
         </div>
@@ -63,29 +68,31 @@ const Main = () => {
                 </p>
               </div>
             </div>
-            <div>
-              <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
-                <div className="flex flex-col justify-center w-full">
-                  <form className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-md" action="/upload" method="POST" encType="multipart/form-data" id='forma' onChange={handlePostMethod}>
-                    <div className="space-y-1 text-center">
-                      <div className="flex flex-col text-sm text-gray-600 w-full">
-                        <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500">
-                          <span>{t('upload')}</span>
-                          <input id="file-upload" name="file" type="file" className="sr-only" />
-                        </label>
-                      </div>
-                      <p className="text-xs text-gray-500">{t('upload-subheading')}</p>
+            <div className="flex justify-center" data-aos="zoom-y-out" data-aos-delay="450">
+              <div className="flex flex-col justify-center w-full">
+                <form className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-t-md" action="/upload" method="POST" encType="multipart/form-data" id='forma' onChange={handlePostMethod}>
+                  <div className="space-y-1 text-center">
+                    <div className="flex flex-col text-sm text-gray-600 w-full">
+                      <label htmlFor="file-upload" className="cursor-pointer bg-white rounded-md font-medium text-blue-600 hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500">
+                        <span>{t('upload')}</span>
+                        <input id="file-upload" name="file" type="file" className="sr-only" />
+                      </label>
                     </div>
-                  </form>
-                </div>
+                    <p className="text-xs text-gray-500">{t('upload-subheading')}</p>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="absolute right-5 bottom-5">
+            {/* Error displaying under upload form */}
+            <div>
               {
-                sizeError && <SizeError error={sizeError} />
+                formatError ? <FormatError error={formatError} /> : null
               }
               {
-                formatError && <FormatError error={formatError} />
+                sizeError ? <SizeError error={sizeError} /> : null
+              }
+              {
+                success ? <SuccessUpload success={success} /> : null
               }
             </div>
           </div>
