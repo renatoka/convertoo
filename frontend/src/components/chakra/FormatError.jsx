70995369d36d2edc1708ff7a355bcfc69ed4babf
Renato <rkauric@vvg.hr>
2022-11-28 16:25:24 +0100

    Removed unused state import

--- frontend/src/components/chakra/FormatError.jsx
+++ frontend/src/components/chakra/FormatError.jsx
@@ -1,16 +1,15 @@
 import { Alert, AlertIcon, AlertTitle, AlertDescription, SlideFade, CloseButton } from "@chakra-ui/react"
-import { useState } from "react"
 
 const FormatError = ({ error }) => {
   return (
-    <SlideFade in={error} offsetY="20px">
+    <SlideFade in={error}>
       <Alert status="error" variant="subtle" flexDirection="row" alignItems="center" justifyContent="center" textAlign="center" height="50px">
         <AlertIcon />
         <AlertTitle>
           Error
         </AlertTitle>
         <AlertDescription maxWidth="sm">
-          File format is not supported
+          File format is not supported.
         </AlertDescription>
       </Alert>
     </SlideFade>
--- frontend/src/components/chakra/SizeError.jsx
+++ frontend/src/components/chakra/SizeError.jsx
@@ -1,16 +1,15 @@
-import { Alert, AlertIcon, AlertTitle, AlertDescription, SlideFade, CloseButton } from "@chakra-ui/react"
-import { useState } from "react"
+import { Alert, AlertIcon, AlertTitle, AlertDescription, SlideFade } from "@chakra-ui/react"
 
 const SizeError = ({ error }) => {
   return (
-    <SlideFade in={error} offsetY="20px">
-      <Alert status="error" variant="subtle" flexDirection="row" alignItems="center" justifyContent="center" textAlign="center" height="50px">
+    <SlideFade in={error} direction="top">
+      <Alert status="error" variant="subtle" flexDirection="row" alignItems="center" justifyContent="center" textAlign="center" height="50px" className="rounded-b-lg">
         <AlertIcon />
         <AlertTitle>
           Error
         </AlertTitle>
         <AlertDescription maxWidth="sm">
-          Max file size is 10MB
+          Max file size is 10MB.
         </AlertDescription>
       </Alert>
     </SlideFade>
