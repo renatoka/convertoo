715c24c149216e3e8ff0cdf3aa16210132fa5824
Renato <rkauric@vvg.hr>
2022-11-29 14:18:38 +0100

    Added Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Bottom from "./components/Bottom";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <Main />
-      <Bottom />
+      <Footer />
     </div>
   );
 }
