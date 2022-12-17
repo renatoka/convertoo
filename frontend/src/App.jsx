b7fce8201061b3108f23747a506a0f128cb7097b
Renato <rkauric@vvg.hr>
2022-12-17 19:12:53 +0100

    Imported Feature section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Main from "./components/Main";
+import Feature from "./components/Feature";
 import Layout from "./components/Layout/Layout";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div className="App">
       <Layout>
         <Main />
+        <Feature />
       </Layout>
     </div>
   );
